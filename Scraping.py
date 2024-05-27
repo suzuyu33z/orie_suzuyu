53af2fa446d3cbc53698df4ba7250244dd9d0aa1
suzuki <suzuyu33z@gmail.com>
2024-05-27 22:12:15 +0900

    Update with latest changes

--- Scraping.py
+++ Scraping.py
@@ -1,75 +1,91 @@
 import requests
 import re
+import os
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+from dotenv import load_dotenv
 import sqlite3
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 import time
 
-# homesからのスクレイピング（港区の物件のみ）
-base_url = "https://www.homes.co.jp/chintai/tokyo/minato-city/list/?page={}"
-max_page = 5
+# .envファイルを読み込む
+load_dotenv()  # 追加
+
+# 環境変数からAPIキーを取得
+API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+
+# homesからのスクレイピング（複数の地域）
+#１建物の違う部屋を取得できていない・・・
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# homesからのスクレイピング（複数の地域）
+base_url_template = "https://www.homes.co.jp/chintai/tokyo/{}/list/?page={}"
+districts = ["minato-city", "chiyoda-city", "chuo-city", "shinjuku-city", "shibuya-city", "meguro-city"]
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
-
+max_page = 5
 all_data = []
 
-for page in range(1, max_page + 1):
-    url = base_url.format(page)
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'lxml')
-    items = soup.findAll("div", {"class": "mod-mergeBuilding--rent--photo"})
+for district in districts:
+    for page in range(1, max_page + 1):
+        url = base_url_template.format(district, page)
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'lxml')
+        items = soup.findAll("div", {"class": "mod-mergeBuilding--rent--photo"})
 
-    for item in items:
-        base_data = {}
-        base_data["名称"] = item.find(class_="bukkenName").get_text(strip=True) if item.find(class_="bukkenName") else None
-        base_data["アドレス"] = next((td.find_next_sibling('td').get_text(strip=True) for td in item.find_all('th') if td.get_text(strip=True) == '所在地'), None)
-        base_data["アクセス"] = item.find("td", class_="traffic").get_text(strip=True) if item.find("td", class_="traffic") else ', '.join(span.get_text(strip=True) for span in item.find_all("span", class_="prg-stationText"))
-        module_body = item.find('div', class_='moduleBody')
-        if module_body:
-            construction_th = module_body.find('th', string='築年数/階数')
-            if construction_th:
-                base_data["築年数"] = construction_th.find_next_sibling('td').get_text(strip=True).split(' ')[0]
-                base_data["構造"] = construction_th.find_next_sibling('td').get_text(strip=True).split(' ')[2]
+        print("District", district, "Page", page, "Items", len(items))
 
-        rooms = item.find_all(class_="unitListBody prg-unitListBody")
-        for room in rooms:
-            data = base_data.copy()
-            floor_number_td = room.find(class_="roomKaisuu")
-            data["階数"] = floor_number_td.get_text(strip=True) if floor_number_td else None
-
-            rent_price_label = room.select_one("span.priceLabel")
-            rent_price = rent_price_label.get_text(strip=True) if rent_price_label else None
-            rent_admin = rent_price_label.next_sibling.strip().replace("/", "").replace(",", "") if rent_price_label else None
-            data["家賃"] = rent_price
-            data["管理費"] = rent_admin
-
-            price = room.select_one("td.price")
-            br_tag = price.find('br').next_sibling.strip()
-            depo = br_tag.split("/")[0]
-            key = br_tag.split("/")[1]
-            data["敷金"] = depo
-            data["礼金"] = key
-
-            layout = room.select_one("td.layout")
-            room_type = layout.contents[0].strip() if layout.contents else None
-            room_area = layout.find('br').next_sibling.strip().replace('m²', 'm2') if layout.find('br') else None
-            data["間取り"] = room_type
-            data["面積"] = room_area
-
-            property_image_element = item.select_one(".bukkenPhoto .photo img")
-            data["物件画像URL"] = property_image_element["data-original"] if property_image_element else None
-
-            floor_plan_image_element = item.select_one(".floarPlanPic img")
-            data["間取画像URL"] = floor_plan_image_element["data-original"] if floor_plan_image_element else None
-
-            property_link_element = item.select_one("a[href*='/chintai/room']")
-            data["物件詳細URL"] = property_link_element['href'] if property_link_element else None
-            
-            all_data.append(data)
+        for item in items:
+            base_data = {}
+            base_data["名称"] = item.find(class_="bukkenName").get_text(strip=True) if item.find(class_="bukkenName") else None
+            base_data["アドレス"] = next((td.find_next_sibling('td').get_text(strip=True) for td in item.find_all('th') if td.get_text(strip=True) == '所在地'), None)
+            base_data["アクセス"] = item.find("td", class_="traffic").get_text(strip=True) if item.find("td", class_="traffic") else ', '.join(span.get_text(strip=True) for span in item.find_all("span", class_="prg-stationText"))
+            module_body = item.find('div', class_='moduleBody')
+            if module_body:
+                construction_th = module_body.find('th', string='築年数/階数')
+                if construction_th:
+                    base_data["築年数"] = construction_th.find_next_sibling('td').get_text(strip=True).split(' ')[0]
+                    base_data["構造"] = construction_th.find_next_sibling('td').get_text(strip=True).split(' ')[2]
+            rooms = item.find_all(class_="unitListBody prg-unitListBody")
+            for room in rooms:
+                data = base_data.copy()
+                # 当該部屋の階数
+                floor_number_td = room.find(class_="roomKaisuu")
+                data["階数"] = floor_number_td.get_text(strip=True) if floor_number_td else None
+                # 賃料と管理費を分けて取得
+                rent_price_label = room.select_one("span.priceLabel")
+                rent_price = rent_price_label.get_text(strip=True) if rent_price_label else None
+                rent_admin = rent_price_label.next_sibling.strip().replace("/", "").replace(",", "") if rent_price_label else None
+                data["家賃"] = rent_price
+                data["管理費"] = rent_admin
+                # 敷金と礼金を分けて取得
+                price = room.select_one("td.price")
+                br_tag = price.find('br').next_sibling.strip() if price.find('br') else ''
+                depo = br_tag.split("/")[0] if '/' in br_tag else ''
+                key = br_tag.split("/")[1] if '/' in br_tag else ''
+                data["敷金"] = depo
+                data["礼金"] = key
+                # 間取りと占有面積を分けて取得
+                layout = room.select_one("td.layout")
+                if layout and layout.find('br'):
+                    room_type = layout.contents[0].strip() if layout.contents else None
+                    room_area = layout.find('br').next_sibling.strip().replace('m²', 'm2') if layout.find('br') else None
+                    data["間取り"] = room_type
+                    data["面積"] = room_area
+                property_image_element = item.select_one(".bukkenPhoto .photo img")
+                data["物件画像URL"] = property_image_element["data-original"] if property_image_element else None
+                # 間取り画像URL
+                floor_plan_image_element = item.select_one(".floarPlanPic img")
+                data["間取画像URL"] = floor_plan_image_element["data-original"] if floor_plan_image_element else None
+                # 物件詳細URL
+                property_link_element = item.select_one("a[href*='/chintai/room']")
+                data["物件詳細URL"] = property_link_element['href'] if property_link_element else None
+                all_data.append(data)
 
 df = pd.DataFrame(all_data)
 
@@ -104,53 +120,59 @@ def remove_numbers(address):
 df['アドレス_数字除去'] = df['アドレス'].apply(remove_numbers)
 
 # スーモからの情報取得
-base_url = "https://suumo.jp/chintai/tokyo/sc_minato/?page={}"
-max_page = 5
+# 基本URLテンプレートと地域のリスト(間取り画像がなぜか取れない・・・)
+base_url_template = "https://suumo.jp/chintai/tokyo/{}/?page={}"
+districts = ["sc_minato", "sc_chiyoda", "sc_chuo", "sc_shinjuku", "sc_shibuya", "sc_meguro"]
+max_page = 5  # 最大ページ数
 
 all_data = []
 
-for page in range(1, max_page + 1):
-    url = base_url.format(page)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'lxml')
-    items = soup.findAll("div", {"class": "cassetteitem"})
+for district in districts:
+    for page in range(1, max_page + 1):
+        url = base_url_template.format(district, page)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        items = soup.findAll("div", {"class": "cassetteitem"})
 
-    print("page", page, "items", len(items))
+        print(f"{district} - page {page} - items {len(items)}")
 
-    for item in items:
-        base_data = {}
-        base_data["名称"] = item.find("div", {"class": "cassetteitem_content-title"}).get_text(strip=True) if item.find("div", {"class": "cassetteitem_content-title"}) else None
-        base_data["カテゴリ"] = item.find("div", {"class": "cassetteitem_content-label"}).span.get_text(strip=True) if item.find("div", {"class": "cassetteitem_content-label"}) else None
-        base_data["アドレス"] = item.find("li", {"class": "cassetteitem_detail-col1"}).get_text(strip=True) if item.find("li", {"class": "cassetteitem_detail-col1"}) else None
-        base_data["アクセス"] = ", ".join([station.get_text(strip=True) for station in item.findAll("div", {"class": "cassetteitem_detail-text"})])
+        for item in items:
+            base_data = {}
+            base_data["名称"] = item.find("div", {"class": "cassetteitem_content-title"}).get_text(strip=True) if item.find("div", {"class": "cassetteitem_content-title"}) else None
+            base_data["カテゴリ"] = item.find("div", {"class": "cassetteitem_content-label"}).span.get_text(strip=True) if item.find("div", {"class": "cassetteitem_content-label"}) else None
+            base_data["アドレス"] = item.find("li", {"class": "cassetteitem_detail-col1"}).get_text(strip=True) if item.find("li", {"class": "cassetteitem_detail-col1"}) else None
+            # 駅のアクセス情報をまとめて取得
+            base_data["アクセス"] = ", ".join([station.get_text(strip=True) for station in item.findAll("div", {"class": "cassetteitem_detail-text"})])
 
-        construction_info = item.find("li", {"class": "cassetteitem_detail-col3"}).find_all("div") if item.find("li", {"class": "cassetteitem_detail-col3"}) else None
-        base_data["築年数"] = construction_info[0].get_text(strip=True) if construction_info and len(construction_info) > 0 else None
-        base_data["構造"] = construction_info[1].get_text(strip=True) if construction_info and len(construction_info) > 1 else None
+            construction_info = item.find("li", {"class": "cassetteitem_detail-col3"}).find_all("div") if item.find("li", {"class": "cassetteitem_detail-col3"}) else None
+            base_data["築年数"] = construction_info[0].get_text(strip=True) if construction_info and len(construction_info) > 0 else None
+            base_data["構造"] = construction_info[1].get_text(strip=True) if construction_info and len(construction_info) > 1 else None
 
-        tbodys = item.find("table", {"class": "cassetteitem_other"}).findAll("tbody")
+            tbodys = item.find("table", {"class": "cassetteitem_other"}).findAll("tbody")
 
-        for tbody in tbodys:
-            data = base_data.copy()
-            floor_info = tbody.find_all("td")[2].get_text(strip=True) if len(tbody.find_all("td")) > 2 else None
-            data["階数"] = floor_info
-            data["家賃"] = tbody.select_one(".cassetteitem_price--rent").get_text(strip=True) if tbody.select_one(".cassetteitem_price--rent") else None
-            data["管理費"] = tbody.select_one(".cassetteitem_price--administration").get_text(strip=True) if tbody.select_one(".cassetteitem_price--administration") else None
-            data["敷金"] = tbody.select_one(".cassetteitem_price--deposit").get_text(strip=True) if tbody.select_one(".cassetteitem_price--deposit") else None
-            data["礼金"] = tbody.select_one(".cassetteitem_price--gratuity").get_text(strip=True) if tbody.select_one(".cassetteitem_price--gratuity") else None
-            data["間取り"] = tbody.select_one(".cassetteitem_madori").get_text(strip=True) if tbody.select_one(".cassetteitem_madori") else None
-            data["面積"] = tbody.select_one(".cassetteitem_menseki").get_text(strip=True) if tbody.select_one(".cassetteitem_menseki") else None
+            for tbody in tbodys:
+                data = base_data.copy()
+                # 階数情報の正確な取得
+                floor_info = tbody.find_all("td")[2].get_text(strip=True) if len(tbody.find_all("td")) > 2 else None
+                data["階数"] = floor_info
+                data["家賃"] = tbody.select_one(".cassetteitem_price--rent").get_text(strip=True) if tbody.select_one(".cassetteitem_price--rent") else None
+                data["管理費"] = tbody.select_one(".cassetteitem_price--administration").get_text(strip=True) if tbody.select_one(".cassetteitem_price--administration") else None
+                data["敷金"] = tbody.select_one(".cassetteitem_price--deposit").get_text(strip=True) if tbody.select_one(".cassetteitem_price--deposit") else None
+                data["礼金"] = tbody.select_one(".cassetteitem_price--gratuity").get_text(strip=True) if tbody.select_one(".cassetteitem_price--gratuity") else None
+                data["間取り"] = tbody.select_one(".cassetteitem_madori").get_text(strip=True) if tbody.select_one(".cassetteitem_madori") else None
+                data["面積"] = tbody.select_one(".cassetteitem_menseki").get_text(strip=True) if tbody.select_one(".cassetteitem_menseki") else None
 
-            property_image_element = item.find(class_="cassetteitem_object-item")
-            data["物件画像URL"] = property_image_element.img["rel"] if property_image_element and property_image_element.img else None
+                # 物件画像・間取り画像・詳細URLの取得を最後に行う
+                property_image_element = item.find("div", class_="cassetteitem_object-item")
+                data["物件画像URL"] = property_image_element.img.get("rel") if property_image_element and property_image_element.img else None
 
-            floor_plan_image_element = item.find(class_="casssetteitem_other-thumbnail")
-            data["間取画像URL"] = floor_plan_image_element.img["rel"] if floor_plan_image_element and floor_plan_image_element.img else None
+                floor_plan_image_element = tbody.find("div", class_="cassetteitem_other-thumbnail")
+                data["間取画像URL"] = floor_plan_image_element.img.get("rel") if floor_plan_image_element and floor_plan_image_element.img else None
 
-            property_link_element = item.select_one("a[href*='/chintai/jnc_']")
-            data["物件詳細URL"] = "https://suumo.jp" + property_link_element['href'] if property_link_element else None
+                property_link_element = item.select_one("a[href*='/chintai/jnc_']")
+                data["物件詳細URL"] = "https://suumo.jp" + property_link_element['href'] if property_link_element else None
 
-            all_data.append(data)
+                all_data.append(data)
 
 df2 = pd.DataFrame(all_data)
 
@@ -307,33 +329,22 @@ def split_access(row):
 
 df = df.join(df.apply(split_access, axis=1))
 
-# ジオコーダーの初期化
-geolocator = Nominatim(user_agent="your_app_name", timeout=10)
+# Google Maps APIを使用してジオコーディング
+def get_geocode_from_name(address):
+    address = address.replace(" ", "+")  # 空白をプラスに置き換える
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
 
-current_count = 0
-total_count = len(df['アドレス'])
+    if data['status'] == "OK":
+        location = data['results'][0]['geometry']['location']
+        lat = location['lat']
+        lng = location['lng']
+        return lat, lng
+    else:
+        return None, None
 
-def get_lat_lon(address, retries=3):
-    global current_count
-    current_count += 1
-    
-    for attempt in range(retries):
-        try:
-            location = geolocator.geocode(address)
-            if location:
-                print(f"{current_count}/{total_count} 件目実施中 結果: {location.latitude}, {location.longitude}")
-                return location.latitude, location.longitude
-            else:
-                print(f"{current_count}/{total_count} 件目実施中 結果: 住所が見つかりません")
-                return None, None
-        except (GeocoderTimedOut, GeocoderServiceError) as e:
-            print(f"Error retrieving location for address {address}: {e}. Retrying ({attempt + 1}/{retries})...")
-            time.sleep(1)
-
-    print(f"Failed to retrieve location for address {address} after {retries} retries")
-    return None, None
-
-df['緯度'], df['経度'] = zip(*df['アドレス'].apply(get_lat_lon))
+df['緯度'], df['経度'] = zip(*df['アドレス'].apply(get_geocode_from_name))
 
 # SQLiteに格納
 conn = sqlite3.connect('chintai.db')
